173492e1efdfd942aa384dd0455881d1462f0633
MaksYig <yigal.maksimov@gmail.com>
2021-01-07 18:17:45 +0200

    settings cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -20,6 +20,7 @@ const createSendToken = (user, statusCode, req, res) => {
       Date.now() + process.env.JWT_COOKIE_EXP_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    secure: false, //set to true if your using https
   };
   // if (req.secure || req.headers('x-forwarded-proto') === 'https') {
   //   cookieOptions.secure = true;
