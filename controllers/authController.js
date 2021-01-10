3462b3e8e6c5172f82340023d24e8058d8988dea
MaksYig <yigal.maksimov@gmail.com>
2021-01-10 17:32:49 +0200

    .

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,9 +19,10 @@ const createSendToken = (user, statusCode, req, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXP_IN * 24 * 60 * 60 * 1000
     ),
-    sameSite: false,
+    sameSite: true,
     httpOnly: true,
-    secure: req.secure || req.headers('x-forwarded-proto') === 'https',
+    secure: true,
+    // secure: req.secure || req.headers('x-forwarded-proto') === 'https',
   };
 
   /* (req.secure || req.headers('x-forwarded-proto') === 'https') - use this only when web online */
